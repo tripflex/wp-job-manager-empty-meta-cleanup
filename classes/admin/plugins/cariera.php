f1b74b8a699e148ac93e7c4c76922eee6f027d19
Myles McNamara <myles@smyl.es>
2024-02-19 09:20:48 -0500

    add support for Cariera 1.7.2+ company fields fixes #5

--- classes/admin/plugins/cariera.php
+++ classes/admin/plugins/cariera.php
@@ -37,15 +37,33 @@ class Cariera extends MetaRemove {
 	 */
 	public function get_fields() {
 
-		if ( ! class_exists( 'Cariera_Company_Manager_Writepanels' ) ) {
-
-			if ( ! defined( 'CARIERA_PLUGIN_DIR' ) ) {
-				return [];
-			}
-
-			require_once( CARIERA_PLUGIN_DIR . '/inc/core/wp-company-manager/writepanels.php' );
+		if ( ! defined( 'CARIERA_PLUGIN_DIR' ) ) {
+			return [];
 		}
 
-		return \Cariera_Company_Manager_Writepanels::company_fields();
+		/**
+		 * Version >= 1.7.2+ of Cariera changes to using namespaces
+		 */
+		if ( ! class_exists( '\Cariera_Core\Core\Company_Manager\Writepanels' ) ) {
+			// Check for the existence of the old class name
+			if ( class_exists( 'Cariera_Company_Manager_Writepanels' ) ) {
+				class_alias( 'Cariera_Company_Manager_Writepanels', '\Cariera_Core\Core\Company_Manager\Writepanels' );
+			} else {
+
+				// Define the new and old file paths
+				$new_file_path = CARIERA_PLUGIN_DIR . '/inc/core/company-manager/writepanels.php';
+				$old_file_path = CARIERA_PLUGIN_DIR . '/inc/core/wp-company-manager/writepanels.php';
+
+				// Check if the new file exists
+				if ( file_exists( $new_file_path ) ) {
+					require_once( $new_file_path );
+				} // If the new file doesn't exist, check if the old file exists
+				else if ( file_exists( $old_file_path ) ) {
+					require_once( $old_file_path );
+				}
+			}
+		}
+
+		return \Cariera_Core\Core\Company_Manager\Writepanels::company_fields();
 	}
 }
